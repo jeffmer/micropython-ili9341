0a5191115793691c0c47aaccf239c41face24ba1
jeffmer <jnm2@Jeff-Magees-Air.local>
2018-01-06 12:57:45 +0000

    Initial Committ

---
+++ main.py
@@ -0,0 +1,38 @@
+# test of printing multiple fonts to the ILI9341 on an M5Stack using H/W SP
+# MIT License; Copyright (c) 2017 Jeffrey N. Magee
+
+from ili934xnew import ILI9341, color565
+from machine import Pin, SPI
+import m5stack
+import tt14
+import glcdfont
+import tt14
+import tt24
+import tt32
+
+fonts = [glcdfont,tt14,tt24,tt32]
+
+text = 'Now is the time for all good men to come to the aid of the party.'
+
+power = Pin(m5stack.TFT_LED_PIN, Pin.OUT)
+power.value(1)
+
+spi = SPI(
+    2,
+    baudrate=40000000,
+    miso=Pin(m5stack.TFT_MISO_PIN),
+    mosi=Pin(m5stack.TFT_MOSI_PIN),
+    sck=Pin(m5stack.TFT_CLK_PIN))
+
+display = ILI9341(
+    spi,
+    cs=Pin(m5stack.TFT_CS_PIN),
+    dc=Pin(m5stack.TFT_DC_PIN),
+    rst=Pin(m5stack.TFT_RST_PIN))
+display.erase()
+display.set_pos(0,0)
+for ff in fonts:
+    display.set_font(ff)
+    display.print(text)
+
+
